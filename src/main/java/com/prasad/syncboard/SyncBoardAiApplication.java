15bdf3adba6649f4adc1ce8bc93fdf14e96dcb40
Prasad Ganesh Dongare <prasaddongare1601@gmail.com>
2026-02-13 14:21:55 -0700

    Initial commit: Setup project structure and Task entity

---
+++ src/main/java/com/prasad/syncboard/SyncBoardAiApplication.java
@@ -0,0 +1,13 @@
+package com.prasad.syncboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncBoardAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncBoardAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prasad/syncboard/SyncBoardAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasad.syncboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncBoardAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
